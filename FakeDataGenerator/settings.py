8fbe14e62bf737cdd975a23b8178829bb69b98c8
Valg1s <antonsegodin@gmail.com>
2023-03-17 09:36:58 +0100

    Deploy

--- FakeDataGenerator/settings.py
+++ FakeDataGenerator/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0(3djta2x!veybz+hezjoi1qum-d3xyzp)51$@h%*!#j@!zvsc'
+SECRET_KEY = os.environ.get("SECRET_KEY","some_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "some_debug")
 
 ALLOWED_HOSTS = []
 
@@ -127,9 +131,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 
-TIMEOUT = 600
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = '/var/www/valg1s/static'
